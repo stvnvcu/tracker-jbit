59744887a0aa94b986e6cf8bf15410b28d10ec99
stevanovic.uros <stevanovic.uros@credo-company.com>
2025-03-27 15:17:55 +0100

    Init project

---
+++ src/main/java/com/jbit/trackerjbit/TrackerjbitApplication.java
@@ -0,0 +1,13 @@
+package com.jbit.trackerjbit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackerjbitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrackerjbitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jbit/trackerjbit/TrackerjbitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jbit.trackerjbit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackerjbitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
