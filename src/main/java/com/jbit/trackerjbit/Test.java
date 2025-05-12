7f489e711e0a681ee71bbd25ef63e41073c5dc6f
stevanovic.uros <stevanovic.uros@credo-company.com>
2025-05-12 22:13:01 +0200

    Add test class

---
+++ src/main/java/com/jbit/trackerjbit/Test.java
@@ -0,0 +1,16 @@
+package com.jbit.trackerjbit;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class Test {
+
+    @GetMapping
+    public String test(){
+        return "Hello World";
+    }
+
+}
